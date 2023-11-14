068030d91715ec3ce88bd3e856757dfaf29c97c5
Alam Guardin <alamguardin@gmail.com>
2023-11-13 20:26:55 -0500

    style: create home section

---
+++ src/components/Content.jsx
@@ -0,0 +1,11 @@
+import HomeSection from "./sections/HomeSection";
+
+function Content() {
+    return (
+        <div className="content">
+            <HomeSection></HomeSection>
+        </div>
+    )
+}
+
+export default Content;
