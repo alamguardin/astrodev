ae22da76486d68b3889bedeb18dd8204c18b5c27
Alam Guardin <alamguardin@gmail.com>
2023-11-14 21:07:14 -0500

    style: create about section

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,9 +1,11 @@
+import AboutSection from "./sections/AboutSection";
 import HomeSection from "./sections/HomeSection";
 
 function Content() {
     return (
         <div className="content">
             <HomeSection></HomeSection>
+            <AboutSection></AboutSection>
         </div>
     )
 }
